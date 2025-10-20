81f695fea6748f805c5ffb09ab2f87b4829db2fd
MohMohAung <mohmohaung737@gmail.com>
2025-10-20 13:02:46 +0630

    fix:bank-admin-design

--- src/components/common/ProtectedRoute.tsx
+++ src/components/common/ProtectedRoute.tsx
@@ -1,10 +1,11 @@
+import type { RootState } from "@/app/store/store";
 import { useEffect, type ReactNode } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function ProtectedRoute({ children }: { children: ReactNode }) {
   const navigate = useNavigate();
-  const { accessToken } = useSelector((state: any) => state.auth);
+  const { accessToken } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
     if (!accessToken) {
